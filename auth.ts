c7540f63dda9ff16222315c13e28e2a20c262c09
kyaw-starse <101231054+kyaw-starse@users.noreply.github.com>
2025-03-14 09:59:23 +0900

    update for fix

--- auth.ts
+++ auth.ts
@@ -1,42 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { client } from "./sanity/lib/client";
 import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries";
 import { writeClient } from "./sanity/lib/write-client";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+// Define auth configuration separately
+export const authConfig = {
   providers: [GitHub],
   callbacks: {
-    async signIn({
-      user: { name, email, image },
-      profile: { id, login, bio },
-    }) {
+    async signIn({ user, profile }: { user: any; profile: any }) {
       const existingUser = await client
         .withConfig({ useCdn: false })
         .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-          id: id,
+          id: profile?.id,
         });
 
       if (!existingUser) {
         await writeClient.create({
           _type: "author",
-          id,
-          name,
-          username: login,
-          email,
-          image,
-          bio: bio || "",
+          id: profile.id,
+          name: user.name,
+          username: profile.login,
+          email: user.email,
+          image: user.image,
+          bio: profile.bio || "",
         });
       }
 
       return true;
     },
-    async jwt({ token, account, profile }) {
+    async jwt({ token, account, profile }: { token: any; account: any; profile: any }) {
       if (account && profile) {
         const user = await client
           .withConfig({ useCdn: false })
           .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-            id: profile?.id,
+            id: profile.id,
           });
 
         token.id = user?._id;
@@ -44,9 +43,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
       return token;
     },
-    async session({ session, token }) {
-      Object.assign(session, { id: token.id });
+    async session({ session, token }: { session: any; token: any }) {
+      session.id = token.id as string;
       return session;
     },
   },
-});
+};
+
+// Export NextAuth instance using authConfig
+export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
